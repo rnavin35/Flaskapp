ef652da6fa67ab8d517fbd66055d42c550c6b1b8
Navin Raju <navinraju@navins-mbp.lan>
2020-11-20 10:54:50 -0600

    add welcome

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ def sum(parameter1,parameter2):
     add = int(parameter1) + int(parameter2)
     return jsonify(sum=add)
 
+@app.route('/')
+def index():
+    return "<h1>Welcome to our server !!</h1>"
+
 if __name__ == "__main__":
     app.run(debug=True)
